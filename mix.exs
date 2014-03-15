f2d7b9dfd6a7a2f970e16eabeecef91cac6dbd2b
David Rouchy <drouchy@gmail.com>
2014-03-15 07:44:20 +0000

    update the require elixir version & fix a dependency

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exrabbit.Mixfile do
   def project do
     [ app: :exrabbit,
       version: "0.0.1",
-      elixir: "~> 0.10.2-dev",
+      elixir: "~> 0.12.5",
       deps: deps ]
   end
 
@@ -17,7 +17,7 @@ defmodule Exrabbit.Mixfile do
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
-      { :amqp_client, ">0.0", github: "jbrisbin/amqp_client" },
+      { :amqp_client, github: "jbrisbin/amqp_client" },
     ]
   end
 end
