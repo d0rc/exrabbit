7767573123bdfdd54361d5c4093cab9c918422b4
d0rc <7993863@gmail.com>
2014-10-13 03:24:08 +0400

    fix jazz

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Exrabbit.Mixfile do
   defp deps do
     [
       { :amqp_client, github: "d0rc/amqp_client" },
-      { :jazz, github: "d0rc/jazz", branch: "v0.14.1"},
+      { :jazz, github: "meh/jazz"},
       { :sweetconfig, github: "d0rc/sweetconfig"}
     ]
   end
